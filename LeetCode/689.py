5a9ef35dd6fb6c018bf4f878f390ec211a5563d0
chengixr <1311621803@qq.com>
2021-12-08 19:28:51 +0800

    leetcode

---
+++ LeetCode/689.py
@@ -0,0 +1,36 @@
+#689. 三个无重叠子数组的最大和
+
+''''
+给你一个整数数组 nums 和一个整数 k ，找出三个长度为 k 、互不重叠、且 3 * k 项的和最大的子数组，并返回这三个子数组。
+
+以下标的数组形式返回结果，数组中的每一项分别指示每个子数组的起始位置（下标从 0 开始）。如果有多个结果，返回字典序最小的一个。
+'''
+
+def maxSumOfThreeSubarrays(nums, k: int):
+    ans = []
+    sum1, maxSum1, maxSum1Idx = 0, 0, 0
+    sum2, maxSum12, maxSum12Idx = 0, 0, ()
+    sum3, maxTotal = 0, 0
+    for i in range(k * 2, len(nums)):
+        sum1 += nums[i - k * 2]
+        sum2 += nums[i - k]
+        sum3 += nums[i]
+        if i >= k * 3 - 1:
+            if sum1 > maxSum1:
+                maxSum1 = sum1
+                maxSum1Idx = i - k * 3 + 1
+            if maxSum1 + sum2 > maxSum12:
+                maxSum12 = maxSum1 + sum2
+                maxSum12Idx = (maxSum1Idx, i - k * 2 + 1)
+            if maxSum12 + sum3 > maxTotal:
+                maxTotal = maxSum12 + sum3
+                ans = [*maxSum12Idx, i - k + 1]
+            sum1 -= nums[i - k * 3 + 1]
+            sum2 -= nums[i - k * 2 + 1]
+            sum3 -= nums[i - k + 1]
+    return ans
+
+
+nums = [1,2,1,2,6,7,5,1]
+k = 2
+print(maxSumOfThreeSubarrays(nums, k))
